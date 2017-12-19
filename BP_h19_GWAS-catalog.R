a5fe751a71470d0432e3b5a30349a10a2fd2cfd3
Jonas B <jonas.bacelis@gmail.com>
2017-12-19 14:36:02 +0100

    fixed problems with SNP-name extraction and position update

--- BP_h19_GWAS-catalog.R
+++ BP_h19_GWAS-catalog.R
@@ -1,27 +1,108 @@
-load("~/Desktop/Work/GWAS_catalog/gwas_catalog_v1.0-associations_e90_r2017-11-27.Rdata")
+# update GWAS Catalog SNPs by assigning them hg19 positions from UCSC
+# Pol & Jonas. 2017 Dec 15-20
 
+#load("~/Desktop/Work/GWAS_catalog/gwas_catalog_v1.0-associations_e90_r2017-11-27.Rdata") # @Pol
+load("~/Dropbox/GIT/GWAS_CATALOG/gwas_catalog_v1.0-associations_e90_r2017-11-27.Rdata") # @Jon
+
+# preview SNP names
 table(substr(a$SNPS,1,2))
-a$SNPS[which(substr(a$SNPS,1,2)!="rs")]
 
-a= a[grep("rs",a$SNPS),]
+# extract all lines with informative SNP names
+a = a[grep("rs",a$SNPS),]
 
-d= unique((unlist(a$SNPS)))
+# extract SNP names
+snps = unique((unlist(a$SNPS)))
 
-#========================================================================================================
-# Run from here
+# doublecheck whether SNP names look OK
+table(nchar(snps)) # -> there are many written in a string (with multiple SNPs)
+snps[which(nchar(snps)>15)] # non-standard ones
 
-write.table(a,"~/Desktop/Work/GWAS_catalog/snp_list.txt", row.names=F, quote=F) # Use this to retreive bp 
-# from UCSC GRCh37/hg19 
+# examples of common problems:
+"rs6858430 x rs4800250"
+"rs8453; rs926938" # snps[grep(";",snps)]
+"HLA-DQB1*02:01, rs558702" # snps[grep(",",snps)]
 
-s= read.table("~/Desktop/Work/GWAS_catalog/SNP_coordinates.txt",h=T)
-s$name= as.character(s$name)
-s$chrom= as.character(s$chrom)
-s= s[(grep("_", s$chrom,invert=T)),]
-s= s[!duplicated(s$name),]
-s= s[,c(1,3,4)]
-colnames(s)= c("chr","bp_hg19","snp")
+### separate into batches:
 
-s$chr=gsub('chr', '', s$chr)
+# pure good-old-fashioned rs SNPs
+ix = grep("^rs[0-9]+$",snps)
+gr1 = snps[ix]
+snps = snps[-ix]
 
-write.table(s,"~/Desktop/Work/GWAS_catalog/snplist_bp_hg19.txt", row.names=F, quote=F, sep="\t")
+# those separated by ";"
+ix = grep("^rs[0-9]+;.*rs[0-9]+$",snps)
+gr2 = snps[ix]
+snps = snps[-ix]
+gr2 = unique(unlist(lapply(gr2,function(x) unlist(strsplit(x,"; ")))))
+
+# those separated by "x"
+ix = grep("^rs[0-9]+ x *rs[0-9]+$",snps)
+gr3 = snps[ix]
+snps = snps[-ix]
+gr3 = unique(unlist(lapply(gr3,function(x) unlist(strsplit(x," x ")))))
+
+# those separated by ","
+ix = grep(",",snps)
+gr4 = snps[ix]
+snps = snps[-ix]
+gr4 = unique(unlist(lapply(gr4,function(x) unlist(strsplit(x,", ")))))
+
+# remaining ones separated by ";"
+ix = grep(";",snps)
+gr5 = snps[ix]
+snps = snps[-ix]
+gr5 = unique(unlist(lapply(gr5,function(x) unlist(strsplit(x,"; ")))))
+
+# there is one ambiguous/nonstandard SNP name remaining
+snps  # we can safely get rid of this one..
+
+# pre-pre-final list of SNP names
+snps = unique(c(gr1,gr2,gr3,gr4,gr5))
+
+# number of still problematic instances
+length(snps) - length(grep("rs[0-9]+$",snps))
+
+# pre-final list of SNP names
+ix = grep("rs[0-9]+$",snps)
+snps = snps[ix]
+
+# still problematic instances
+table(nchar(snps)) # "rs8"? wtf..
+snps = snps[which(nchar(snps)>3)]
+
+# final check
+table(nchar(snps)) # looks good!
+
+# final-final
+snps = unique(snps)
+
+# export to UCSC browser
+#write.table(snps,"~/Desktop/Work/GWAS_catalog/temp_GWAScatalog_snpList.txt", row.names=F, quote=F) # @Pol
+write.table(snps,"~/Downloads/temp_GWAScatalog_snpList.txt", row.names=F, quote=F) # @Jon
+
+
+#=====================================================
+...
+... upload to UCSC and extract GRCh37/hg19 coordinates
+...
+#=====================================================
+
+# load the export from UCSC
+#s = read.table("~/Desktop/Work/GWAS_catalog/temp_GWAScatalog_snpPositions_hg19.txt",h=T,sep="\t",stringsAsFactors = F) # @Pol
+s = read.table("~/Downloads/temp_GWAScatalog_snpPositions_hg19.txt",h=T,stringsAsFactors = F,sep="\t") # @Jon
+
+# eliminate nonsense
+s = s[which(nchar(s$chrom) %in% c(4,5)),] # no haplo blocks (non-chromosomes)
+
+# recode
+s$chrom = gsub('chr', '', s$chrom)
+s$chrom[which(s$chrom %in% c("x","X"))] = 23
+s$chrom = as.numeric(s$chrom)
+
+# reformat
+s = s[,c("chrom","name","chromEnd")]
+
+# save
+#save(list="s", file="~/Desktop/Work/GWAS_catalog/e90_r2017-11-27_hg19positions.Rdata") # @Pol
+save(list="s", file="~/Dropbox/GIT/GWAS_CATALOG/e90_r2017-11-27_hg19positions.Rdata") # @Jon
 
